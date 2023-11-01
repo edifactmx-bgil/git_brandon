4c9423788c4ca311368ebbf0aaf28bfaa053e424
brandongilcaamal <edifactmxbrandongil@gmail.com>
2023-11-01 13:17:16 -0600

    Modificando variable parrafo_1 e imprimiendo en index.php

--- global_configuration.php
+++ global_configuration.php
@@ -3,7 +3,7 @@ $name = 'App GitHub';
 $user_name = 'root';
 $email = 'user@example.com';
 $password = '';
-$parrafo_1 = 'Este ejemplo';
+$parrafo_1 = 'Este es el ejemplo de un párrafo obtenido desde archivo de configuración';
 
 if(file_exists('configuration.php')) {
 	include_once('configuration.php');
