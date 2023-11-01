0f2368d990ed99523df786b8ffb5a82e3ed0734e
brandongilcaamal <edifactmxbrandongil@gmail.com>
2023-11-01 13:16:19 -0600

    Agregando nueva variable e imprimiendo en index.php

--- global_configuration.php
+++ global_configuration.php
@@ -3,6 +3,7 @@ $name = 'App GitHub';
 $user_name = 'root';
 $email = 'user@example.com';
 $password = '';
+$parrafo_1 = 'Este ejemplo';
 
 if(file_exists('configuration.php')) {
 	include_once('configuration.php');
